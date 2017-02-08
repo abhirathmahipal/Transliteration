d355fac6a5f1df25db6937afc677e1cd3d7aa846
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2017-02-08 13:27:56 +0530

    Layout

---
+++ NETStandard/Transliteration/Transliteration/Translator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Transliteration
+{
+    public class Translator
+    {
+        string text_to_translate, text_in_target_language;
+        // Dictionary for the language map
+
+        Translator(string text, string target_lang)
+        {
+            // Try finding target lang without having the user to input
+            // if the language map does not exist, (say Eng -> Tamil)
+            // notify the user.
+        }
+        
+        string translate()
+        {
+            return text_in_target_language;
+        }
+        
+        // string detect_source_lang() 
+        // bool does_map_exist()
+
+    }
+}
