8b9ba50b6835584e2c7a092fb4947b075e9d5575
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2017-02-13 15:53:13 +0530

    Translator class modified and small sample application

--- NETStandard/Transliteration/Transliteration/Translator.cs
+++ NETStandard/Transliteration/Transliteration/Translator.cs
@@ -7,23 +7,32 @@ namespace Transliteration
 {
     public class Translator
     {
-        string text_to_translate, text_in_target_language;
-        // Dictionary for the language map
-
-        Translator(string text, string target_lang)
+        Translator(string target_lang)
         {
-            // Try finding target lang without having the user to input
+            // Try finding source lang without having the user to input
             // if the language map does not exist, (say Eng -> Tamil)
             // notify the user.
         }
         
-        string translate()
+        string translate(string text_to_translate)
         {
-            return text_in_target_language;
+            return "text_in_target_language";
         }
         
-        // string detect_source_lang() 
-        // bool does_map_exist()
+        string detect_source_lang()
+        {
+            return "source lang";
+        }
+
+        bool does_map_exist()
+        {
+            return true;
+        }
+
+        /*IDictionary<int, int> get_appropriate_map()
+        {
+           
+        }*/
 
     }
 }
